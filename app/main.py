bb2b916234f0023263e14a00e3992b191b92b376
Aryan Naraghi <aryann@google.com>
2017-01-31 14:13:10 -0800

    Adds logic to auto-generate the set of signatures using a CSV file.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import collections
+import csv
 import os
 
 import jinja2
@@ -8,12 +10,30 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 TEMPLATE = JINJA_ENVIRONMENT.get_template(os.path.join('templates', 'index.html'))
+SIGNATURES_FILE = os.path.join(os.path.dirname(__file__), 'signatures.csv')
+
+Signature = collections.namedtuple('Signature', ['name', 'title', 'company'])
+SIGNATURES_PER_ROW = 3
 
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
+      signatures = []
+      with open(SIGNATURES_FILE) as f:
+        reader = csv.reader(f)
+        curr = []
+        for i, row in enumerate(reader):
+          if i != 0 and i % SIGNATURES_PER_ROW == 0:
+            signatures.append(curr)
+            curr = []
+          curr.append(Signature(name=row[0], title=row[1], company=row[2]))
+
+      if not curr:
+        signatures.append(curr)
+
       template_values = {
+          'signatures': signatures,
       }
       self.response.write(TEMPLATE.render(template_values))
 
