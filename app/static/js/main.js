98778a92a96ff33c44fbfa3d91b70e92a920985d
maniart <mani.art@gmail.com>
2017-01-31 16:25:34 -0500

    Styles

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,20 +1,7 @@
-
-// Fireup the plugins
-$(document).ready(function(){
-	
-	// initialise  slideshow
-	 $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-
-});
 /**
  * Handles toggling the navigation menu for small screens.
  */
-( function() {
+function initialize() {
 	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
 	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
 
@@ -39,4 +26,28 @@ $(document).ready(function(){
 			menu.className += ' toggled-on';
 		}
 	};
-} )();
+};
+
+// Fireup the plugins
+$(document).ready(function(){
+
+	// initialise  slideshow
+	$('.flexslider').flexslider({
+  	animation: "fade",
+  	directionNav: false,
+    start: function(slider){
+      // alert()
+      // $('body').removeClass('loading');
+    }
+  });
+
+	// call initialize when dom is loaded
+
+	// commenting this out for now - `#topnav` does not exits
+	// TODO fix this
+	// initialize();
+
+
+});
+
+
