8a932b288c04bda80ed9c3b923c5eca0db0cef9b
maniart <mani.art@gmail.com>
2017-02-01 11:14:46 -0500

    add modal for form

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,42 +1,17 @@
-/**
- * Handles toggling the navigation menu for small screens.
- */
-function initialize() {
-	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
-	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
-
-	if ( undefined === button )
-		return false;
-
-	// Hide button if menu is missing or empty.
-	if ( undefined === menu || ! menu.childNodes.length ) {
-		button.style.display = 'none';
-		return false;
-	}
-
-	button.onclick = function() {
-		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
-			menu.className = 'srt-menu';
-
-		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
-			button.className = button.className.replace( ' toggled-on', '' );
-			menu.className = menu.className.replace( ' toggled-on', '' );
-		} else {
-			button.className += ' toggled-on';
-			menu.className += ' toggled-on';
-		}
-	};
-};
-
-// Fireup the plugins
 $(document).ready(function(){
 
   var $window = $(window);
   var $flexSlider = $('.flexslider');
-  var $form = $('.form-iframe');
-  var $signBtn = $('.buttonlink')
-  var $backBtn = $('.back')
-	// initialise  slideshow
+  var windowWidth = $window.width();
+  var windowHeight = $window.height();
+	var $venobox = $('.venobox');
+  var mobileBreakPoint = 768;
+
+  function isMobile() {
+    return windowWidth < mobileBreakPoint;
+  }
+
+  // initialise  slideshow
 	$('.flexslider').flexslider({
   	animation: "fade",
     slideshow: true,
@@ -45,33 +20,14 @@ $(document).ready(function(){
     controlNav: false
   });
 
-
-  function initForm() {
-    var windowHeight = $window.height();
-    var windowWidth = $window.width();
-    $form.attr('width', windowWidth);
-    $form.attr('height', windowHeight);
-    $signBtn.on('click', function(e) {
-      e.preventDefault();
-      // $backBtn.addClass('visible');
-      if(windowWidth < 768) {
-        window.open('https://docs.google.com/forms/d/e/1FAIpQLScOH4yMtuYrYIC-AUf1JTHc7bMZgqHRJ6rQ-_HShQGKIi31BQ/viewform');
-      } else {
-        $form.addClass('visible');
-      }
-
-    });
-    $backBtn.on('click', function(e) {
-      // $backBtn.removeClass('visible');
-      $form.removeClass('visible');
-    });
+  // initialize the modal plugin for Form
+  // opens form in new window if on mobile
+  // opens form in modal if on desktop
+  if(isMobile()) {
+    $venobox.attr('target', '_blank');
+  } else {
+    $venobox.venobox();
   }
-	// call initialize when dom is loaded
-
-	// commenting this out for now - `#topnav` does not exits
-	// TODO fix this
-	// initialize();
-  initForm();
 
 });
 
