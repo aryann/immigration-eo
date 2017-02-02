322e420ed895bf7dab741a5a093a2b97bd7fda0a
Aryan Naraghi <aryann@google.com>
2017-02-02 10:05:50 -0800

    Adds logic to pull in the counter value from the Spreadsheet.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import collections
+import httplib
 import csv
 import os
 
 import jinja2
 import webapp2
+
+from google.appengine.api import urlfetch
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(
@@ -16,6 +19,13 @@ SIGNATURES_FILE = os.path.join(os.path.dirname(__file__), 'signatures.csv')
 
 Signature = collections.namedtuple('Signature', ['name', 'company', 'title'])
 SIGNATURES_PER_ROW = 3
+
+
+NUM_SIGNATURES_URL = (
+    'https://docs.google.com/spreadsheets/d/'
+    '17jz1v4DusIRhtQYhkvelHq1oJlsFSBat5aSb0cHNQ2k/'
+    'pub?gid=1523619827&single=true&output=csv')
+NUM_SIGNATURES_FALLBACK = 3000  # We have at least this many signatures.
 
 
 def get_signatures_from_file():
@@ -46,7 +56,19 @@ def get_signatures_from_file():
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
+      num_signatures = NUM_SIGNATURES_FALLBACK
+      try:
+        result = urlfetch.fetch(NUM_SIGNATURES_URL)
+        if result.status_code == httplib.OK:
+          try:
+            num_signatures = int(result.content)
+          except ValueError:
+            pass
+      except urlfetch.Error:
+            pass
+
       template_values = {
+        'num_signatures': '{:,}'.format(num_signatures),
       }
       self.response.write(HOME_TEMPLATE.render(template_values))
 
